20619f8a4be8e3272461fdbb62184fa3e71aa351
Arden <arden.haldorson@gmail.com>
2025-12-05 14:50:41 +0100

    using helper for printing in color updated AskForUserInput to return void and moved DisplayListOfAssets to Program added functionality to add Mobile assets

--- Checkpoint_3/CompanyAssetList.cs
+++ Checkpoint_3/CompanyAssetList.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/* Company Asset List */
 
 namespace Checkpoint_3
 {
@@ -8,35 +8,34 @@ namespace Checkpoint_3
         public List<CompanyAsset> Assets { get; private set; } = new List<CompanyAsset>();
         public List<CompanyAsset> SortedAssets { get; private set; } = new List<CompanyAsset>();
 
-        // LINQ query to sort the assets by office location and purchase date
-        //    SortedAssets = allAssets
-        //        .OrderBy(asset => asset.Office)
-        //        .ThenBy(asset => asset.PurchaseDate)
-        //        .ToList();
+        // I want to show additional text if the user is returning to the start of the loop,
+        // which is why I have a bool to check if it's the user's First time seeing the message
+        private bool _IsFirstTimeRunning = true;
 
         // Method to add a product to the list
-        public string AskForUserInput(List<CompanyOffice> Offices)
+        public void AskForUserInput(List<CompanyOffice> Offices)
         {
+
             while (true)
             {
                 /* Start of Enter a new asset loop */
-                Console.WriteLine("\nStart by choosing which Company Office inventory you would like to add the new product to:");
+                Console.WriteLine($"\nChoose which Company Office inventory you would like to add the new product to {(!_IsFirstTimeRunning ? "or enter \"Q\" to quit (show list)" : "")}");
+                Console.WriteLine($"-------------------------------------------------------------------------------{(!_IsFirstTimeRunning ? "---------------------------------" : "")}");
                 Console.WriteLine("Office 1: New York | Office 2: London | Office 3: Stockholm");
 
-                string UserInput = InputHelper.GetValidatedStringInput("Office Number", new List<string> { "1", "2", "3" }, out bool isQuit);
-
-                var SelectedOffice = Offices.Find(o => o.OfficeId == UserInput);
+                string UserInput = InputHelper.GetValidatedStringInputForField("Company Office", new List<string> { "1", "New York", "2", "London", "3", "Stockholm" }, out bool isQuit);
 
                 if (isQuit)
                 {
-                    DisplayListOfAssets();
-                    Console.WriteLine("Exiting application, no Company Assets added.");
-                    return "Q";
+                    break;
                 }
 
-                if (UserInput == "1") AddCompanyAsset(Offices[0]);
-                if (UserInput == "2") AddCompanyAsset(Offices[1]);
-                if (UserInput == "3") AddCompanyAsset(Offices[2]);
+                // Depending on the user's input they start adding a new CompanyAsset to one of the three offices
+                if (UserInput == "1" || UserInput == "New York") AddCompanyAsset(Offices[0]);
+                if (UserInput == "2" || UserInput == "London") AddCompanyAsset(Offices[1]);
+                if (UserInput == "3" || UserInput == "Stockholm") AddCompanyAsset(Offices[2]);
+
+                _IsFirstTimeRunning = false;
 
             }
         }
@@ -47,19 +46,38 @@ namespace Checkpoint_3
             CompanyAsset NewAsset;
 
             Console.WriteLine($"\nCompany Office {Office.Location}");
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("You can quit at any point by entering \"Q\"");
+            Console.WriteLine("-------------------------");
+            Helper.PrintStatementInColor("You can quit at any point by entering \"Q\"", ConsoleColor.DarkYellow);
             Console.WriteLine("\nWhich kind of Company Asset would you like to add?");
             Console.WriteLine("1. Computer | 2. Mobile ");
 
-            string Category = InputHelper.GetValidatedStringInput("Category", new List<string> { "Computer", "Mobile", "1", "2" }, out bool isQuit);
-            if (isQuit) return;
-            string BrandName = InputHelper.GetValidatedStringInput("Brand Name", out isQuit);
-            if (isQuit) return;
-            string ModelName = InputHelper.GetValidatedStringInput("Model Name", out isQuit);
-            if (isQuit) return;
+            string Category = InputHelper.GetValidatedStringInputForField("Category", new List<string> { "1", "Computer", "2", "Mobile" }, out bool isQuit);
+            if (isQuit)
+            {
+                Helper.PrintStatementInColor("\nReturning to Company Office selection", ConsoleColor.DarkCyan);
+                return;
+            }
+
+            string BrandName = InputHelper.GetValidatedStringInputForField("Brand Name", out isQuit);
+            if (isQuit)
+            {
+                Helper.PrintStatementInColor("\nReturning to Company Office selection", ConsoleColor.DarkCyan);
+                return;
+            }
+
+            string ModelName = InputHelper.GetValidatedStringInputForField("Model Name", out isQuit);
+            if (isQuit)
+            {
+                Helper.PrintStatementInColor("\nReturning to Company Office selection", ConsoleColor.DarkCyan);
+                return;
+            }
+
             DateTime PurchaseDate = InputHelper.GetValidatedDateTimeInput("Purchase Date", out isQuit);
-            if (isQuit) return;
+            if (isQuit)
+            {
+                Helper.PrintStatementInColor("\nReturning to Company Office selection", ConsoleColor.DarkCyan);
+                return;
+            }
 
             // Get and validate product price
             decimal PriceInUSD = 0;
@@ -68,12 +86,13 @@ namespace Checkpoint_3
 
             do
             {
-                string PriceInUSDInput = InputHelper.GetValidatedStringInput("Price in USD", out isQuit);
+                string PriceInUSDInput = InputHelper.GetValidatedStringInputForField("Price in USD", out isQuit);
                 // Check for quit command
                 if (isQuit)
                 {
                     break;
                 }
+                // Check if input is a valid decimal
                 isValidPrice = InputHelper.TryParseDecimal(PriceInUSDInput, out PriceInUSD);
                 if (isValidPrice)
                 {
@@ -81,12 +100,11 @@ namespace Checkpoint_3
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid price entered. Please enter a valid decimal number.");
-                    Console.ForegroundColor = ConsoleColor.White;
+                    Helper.PrintStatementInColor("Invalid price entered. Please enter a valid decimal number.", ConsoleColor.Red);
                 }
 
             } while (!isValidPrice);
+
             if (isQuit) return;
 
             if (Category == "Computer" || Category == "1")
@@ -99,44 +117,27 @@ namespace Checkpoint_3
                     PriceInLocalCurrency,
                     Office
                 );
-                Console.WriteLine("\nNew Company Asset added successfully:");
-                Console.WriteLine($"{NewAsset.ModelName} {NewAsset.PriceInUSD}, {NewAsset.PriceInLocalCurrency}");
+                Helper.PrintStatementInColor("\nNew Company Asset added successfully.", ConsoleColor.Green);
                 Assets.Add(NewAsset);
             }
 
-
-            //else // Mobile
-            //{
-            //    NewAsset = new Mobile
-            //    {
-            //        Office = 
-            //        BrandName = BrandName,
-            //        ModelName = ModelName,
-            //        PurchaseDate = PurchaseDate,
-            //        PriceInUSD = PriceInUSD
-            //    };
-            //}
-
-            if (isQuit)
+            if (Category == "Mobile" || Category == "2")
             {
-                if (Assets.Count > 0)
-                {
-                    Console.WriteLine("\nExiting application, here is the list of Company Assets added:");
-                    //foreach (var asset in Assets)
-                    //{
-                    //    Console.WriteLine(asset.GetAssetInfo());
-                    //}
-                }
-                else
-                {
-                    Console.WriteLine("Exiting application, no Company Assets added.");
-                }
-                return;
+                NewAsset = new Mobile(
+                    BrandName = Helper.FormatStringInput(BrandName),
+                    ModelName = Helper.FormatStringInput(ModelName),
+                    PurchaseDate,
+                    PriceInUSD,
+                    PriceInLocalCurrency,
+                    Office
+                );
 
+                Helper.PrintStatementInColor("\nNew Company Asset added successfully.", ConsoleColor.Green);
+                Assets.Add(NewAsset);
             }
-
         }
 
+        // LINQ query to sort the assets by office location and purchase date
         public static List<CompanyAsset> SortAssets(List<CompanyAsset> assets)
         {
             return assets
@@ -147,27 +148,24 @@ namespace Checkpoint_3
 
         public void DisplayListOfAssets()
         {
+            // Sort assets before presenting the list
             SortedAssets = SortAssets(Assets);
 
             Console.WriteLine("\nList of Company Assets:");
-            Console.WriteLine("-------------------------------------");
-            Console.WriteLine($"{"Type",-15}{"Brand",-15}{"Model",-12} {"Office",-12}{"Purchase Date",-15}{"Price in USD",-15}{"Currency",-15}{"Local price today",-15}");
+            Console.WriteLine("------------------------");
+            Console.WriteLine($"{"Type",-15}{"Brand",-15}{"Model",-11} {"Office",-15}{"Purchase Date",-15}{"Price in USD",-15}{"Currency",-15}{"Local price today",-15}");
+            Console.WriteLine($"{"----",-15}{"-----",-15}{"-----",-12}{"------",-15}{"-------------",-15}{"------------",-15}{"--------",-15}{"-----------------",-15}");
 
-
+            // Loop through list, checking EndOfLife date and applying color as appropiate
             foreach (CompanyAsset asset in SortedAssets)
             {
-                if (Helper.CalculateEndOfLife(asset.PurchaseDate) == "Red")
+                if (Helper.CalculateEndOfLife(asset.PurchaseDate) == "ThreeMonths")
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"{asset.GetType().Name,-15}{asset.BrandName,-15}{asset.ModelName,-12}{asset.Office.Location,-15}{asset.PurchaseDate.ToString("yyyy-MM-dd"),-15}{asset.PriceInUSD,-15}{asset.Office.CurrencyCode,-15}{asset.PriceInLocalCurrency,-15}");
-                    Console.ForegroundColor = ConsoleColor.White;
-
+                    Helper.PrintStatementInColor($"{asset.GetType().Name,-15}{asset.BrandName,-15}{asset.ModelName,-12}{asset.Office.Location,-15}{asset.PurchaseDate.ToString("yyyy-MM-dd"),-15}{asset.PriceInUSD,-15}{asset.Office.CurrencyCode,-15}{asset.PriceInLocalCurrency,-15}", ConsoleColor.Red);
                 }
-                else if (Helper.CalculateEndOfLife(asset.PurchaseDate) == "Yellow")
+                else if (Helper.CalculateEndOfLife(asset.PurchaseDate) == "SixMonths")
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"{asset.GetType().Name,-15}{asset.BrandName,-15}{asset.ModelName,-12}{asset.Office.Location,-15}{asset.PurchaseDate.ToString("yyyy-MM-dd"),-15}{asset.PriceInUSD,-15}{asset.Office.CurrencyCode,-15}{asset.PriceInLocalCurrency,-15}");
-                    Console.ForegroundColor = ConsoleColor.White;
+                    Helper.PrintStatementInColor($"{asset.GetType().Name,-15}{asset.BrandName,-15}{asset.ModelName,-12}{asset.Office.Location,-15}{asset.PurchaseDate.ToString("yyyy-MM-dd"),-15}{asset.PriceInUSD,-15}{asset.Office.CurrencyCode,-15}{asset.PriceInLocalCurrency,-15}", ConsoleColor.Yellow);
                 }
                 else Console.WriteLine($"{asset.GetType().Name,-15}{asset.BrandName,-15}{asset.ModelName,-12}{asset.Office.Location,-15}{asset.PurchaseDate.ToString("yyyy-MM-dd"),-15}{asset.PriceInUSD,-15}{asset.Office.CurrencyCode,-15}{asset.PriceInLocalCurrency,-15}");
             }
